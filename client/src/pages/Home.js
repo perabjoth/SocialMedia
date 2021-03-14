705499c8dd112c338efe6b2b99654ad6ea94a162
Perabjoth Singh Bajwa <perabjothsinghbajwa@gmail.com>
2021-03-14 15:56:30 +0200

    Add ability to post

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,46 +1,40 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {useQuery} from "@apollo/react-hooks";
-import gql from 'graphql-tag';
-import {Grid} from 'semantic-ui-react';
+import {Grid, Transition} from 'semantic-ui-react';
+
+import {AuthContext} from '../context/auth';
 import PostCard from '../components/PostCard';
+import PostForm from '../components/PostForm';
+import {FETCH_POSTS_QUERY} from '../util/graphql';
+
 
 function Home(props) {
-    const {loading, data: {getPosts: posts} ={}} = useQuery(FETCH_POSTS_QUERY);
+    const {user} = useContext(AuthContext);
+    const {loading, data: {getPosts: posts} = {}} = useQuery(FETCH_POSTS_QUERY);
 
     return (
-        <Grid columns={3} >
+        <Grid columns={3}>
             <Grid.Row className="page-title">
                 <h1>Recent Posts</h1>
             </Grid.Row>
             <Grid.Row>
+                {user && (
+                    <Grid.Column>
+                        <PostForm/>
+                    </Grid.Column>
+                )}
                 {loading ?
                     (<h1>Loading Posts...</h1>) :
-                    (posts && posts.map(post =>(
-                        <Grid.Column key={post.id} style={{marginBottom: 20}}>
-                            <PostCard post={post}/>
-                        </Grid.Column>
-                    )))}
+                    (<Transition.Group>
+                        {posts && posts.map(post => (
+                            <Grid.Column key={post.id} style={{marginBottom: 20}}>
+                                <PostCard post={post}/>
+                            </Grid.Column>
+                        ))}
+                    </Transition.Group>)}
             </Grid.Row>
         </Grid>
     );
-}
+};
 
-const FETCH_POSTS_QUERY = gql`
-    {
-        getPosts{
-            id
-            body
-            createdAt
-            username
-            likes{
-                username
-            }
-            comments{
-                id body username createdAt
-            }
-            likeCount
-            commentCount
-        }
-    }
-`
 export default Home;
